29855973b7e7b793296c9b6c8035ba52a42a7faa
Hariel <hariel@hotmail.com>
2021-03-17 12:11:24 -0300

    Desenvolvimento Web ( 17/03/2021 )

---
+++ Curso Desenvolvimento Web ( Udemy )/Fundamentos JavaScript/NodeJS/ModuloCliente(a+b).js
@@ -0,0 +1,14 @@
+const moduloA = require('./ModuloA')
+const moduloB = require('./ModuloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemvindo)
+console.log(moduloA.atelogo)
+console.log(moduloA)
+
+console.log('\n')
+
+console.log(moduloB.bomdia)
+console.log(moduloB.boanoite())
+console.log(moduloB)
+
